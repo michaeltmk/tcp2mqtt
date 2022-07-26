a557f77775d437bee1c9ae97dcc90594ee684acc
michael tse <tse.man.kit.michael@gmail.com>
2022-07-27 00:27:42 +0800

    feat/add fjson to template func

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -5,11 +5,25 @@ import (
 	"text/template"
 
 	"github.com/bytedance/sonic"
+	"github.com/bytedance/sonic/encoder"
 )
+
+// orderedMarshalString marshals a given value into JSON with ordered keys
+func orderedMarshalString(v any) (string, error) {
+	b, err := encoder.Encode(v, encoder.SortMapKeys)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
 
 // ApplyTemplate gets template of an eventID and applies it
 func ApplyTemplate(templateData, data []byte) ([]byte, error) {
-	t, err := template.New("event-template").Parse(string(templateData))
+	t, err := template.New("template").Funcs(
+		template.FuncMap{
+			"fjson": orderedMarshalString,
+		},
+	).Parse(string(templateData))
 	if err != nil {
 		return nil, err
 	}
